e79973e2282aa67dc5e60ad5385e42198c6f3e8a
ckwwingo <99191737+wingck@users.noreply.github.com>
2023-05-31 03:21:22 -0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -16,4 +16,67 @@ function show(){
         }
 
     }
-}
+};
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+    const pagination = document.querySelector('.pagination');
+    let currentSlide = 0;
+  
+    function showSlide(index) {
+      slides.forEach((slide, i) => {
+        if (i === index) {
+          slide.classList.add('active');
+        } else {
+          slide.classList.remove('active');
+        }
+      });
+  
+      updatePagination(index);
+    }
+  
+    function updatePagination(index) {
+      pagination.innerHTML = '';
+  
+      for (let i = 0; i < slides.length; i++) {
+        const button = document.createElement('button');
+        const icon = document.createElement('i');
+        icon.classList.add('fa-solid', 'fa-circle');
+        button.appendChild(icon);
+  
+        button.addEventListener('click', function() {
+          showSlide(i);
+        });
+  
+        if (i === index) {
+          button.classList.add('active');
+        }
+  
+        pagination.appendChild(button);
+      }
+    }
+  
+    function slideToPrev() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+      slides[currentSlide].classList.add('active');
+      updatePagination(currentSlide);
+    }
+  
+    function slideToNext() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add('active');
+      updatePagination(currentSlide);
+    }
+  
+    prevBtn.addEventListener('click', slideToPrev);
+    nextBtn.addEventListener('click', slideToNext);
+  
+    showSlide(currentSlide);
+  });
+  
+  
+  
+  
